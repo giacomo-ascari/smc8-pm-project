e78d0b89c94d744496255f769eb102ae68ee3663
giacomo-ascari <giacomolego@gmail.com>
2024-05-08 21:33:54 +0200

    damped stiff string matlab implementation

--- fdDev/damped_stiff_string.m
+++ fdDev/damped_stiff_string.m
@@ -1,32 +1,45 @@
-% damped stiff string (supported boundary)
+% damped stiff string (simply supported boundary)
  
-N = 50; % length of bar 
-fs = 48000; % sampling rate for ss 
-dur = 3; 
+fs = 44100; % sampling rate for ss 
+dur = 5; 
+f0 = 300; % Hz | f0 = c / (2*L);
  
-% parameter (lambda groups them all) 
-c = 200;
-L = 1;
-kappa = 10;
+% parameters 
+E = 210*(10^9); % Pa, young's modulus
+T = 750; % N, tension
+ro = 7850; % kg/m3, density
+r = 0.001; % m, radius
+A = pi * (r^2); % m2, cross area
+I = pi * (r^4) / 4; % m4, momentum
+
+c = sqrt(T / (ro * A)); % m/s, wave speed (174)
+kappa = sqrt(E * I / (ro * A)); % m2/s, stiffness coeff. (2.585)
+disp(c);
+disp(kappa);
+L = 0.5 * c / f0; % 1 in this example
 s0 = 1;
 s1 = 0.005;
 
 k = 1/fs;
-h = sqrt((c*c*k*k + 4*s1*k + sqrt((c*c*k*k + 4*s1*k)^2 + 16*kappa*kappa*k*k)) / 2);
-
+lower_h = sqrt((c*c*k*k + 4*s1*k + sqrt((c*c*k*k + 4*s1*k)^2 + 16*kappa*kappa*k*k)) / 2);
+N = floor(L/lower_h);
+h = L / N;
 
 lambda = c*k/h;
-mu = kappa*k/(h^2);
+mu = kappa*k/(h*h);
+
+% for simply supported, we need two virtual points
+N_virt = (N + 1) + 2;
 
 % time only scouts +1, 0, -1 
-u_next = zeros(1, N); 
-u_curr = zeros(1, N); 
-u_prev = zeros(1, N); 
+u_next = zeros(1, N_virt); 
+u_curr = zeros(1, N_virt); 
+u_prev = zeros(1, N_virt); 
  
 input = [1, zeros(1,dur*fs-1)]; 
-input_pos = floor(N*0.13); 
+input_pos = floor(N_virt*0.13); 
 output = zeros(1, dur*fs); 
-output_pos = floor(N*0.78); 
+output_pos = floor(N_virt*0.78);
  
 % iteratore over time 
 for n = 1:dur*fs 
@@ -34,21 +47,23 @@ for n = 1:dur*fs
     u_curr(input_pos) = u_curr(input_pos) + input(n); 
      
     % iterate over length 
-    for l = (1+1):(N-1)
-        
+    % for simply supported, l belongs to {1,...,N-1}
+    % but there are virtual points, extending u in space
+    % and boundaries are manual
+    for l = (mymap(1)):(mymap(N-1))
         u_next(l) = (1 / (1+s0*k)) * (...
             + (2 - 2*lambda*lambda - 6*mu*mu - 4*s1*k/(h*h)) * u_curr(l) + ...
             + (lambda*lambda + 4*mu*mu + 2*s1*k/(h*h)) * (u_curr(l+1) + u_curr(l-1)) + ...
-            - mu*mu*(u_curr(l+2) + u_curr(l-2) + ...
+            - mu*mu*(u_curr(l+2) + u_curr(l-2)) + ...
             + (-1 + s0*k + 4*s1*k/(h*h)) * u_prev(l) + ...
-            - 2*s1*k/(h*h) * (u_prev(l+1) + u_prev(l-1)))); 
+            - 2*s1*k/(h*h) * (u_prev(l+1) + u_prev(l-1))); 
     end 
  
     % boundaries 
-    u_next(1) = 0; 
-    u_next(2) = -u_next(3); 
-    u_next(N-1) = -u_next(N-2); 
-    u_next(N) = 0; 
+    u_next(mymap(0)) = 0;
+    u_next(mymap(-1)) = -u_next(mymap(1)); 
+    u_next(mymap(N)) = 0; 
+    u_next(mymap(N+1)) = -u_next(mymap(N-1)); 
  
     output(n) = u_next(output_pos); 
      
@@ -60,3 +75,10 @@ end
 plot(output); 
  
 soundsc(output, fs);
+
+
+
+
+function y = mymap(x)
+	y = x + 2;
+end
